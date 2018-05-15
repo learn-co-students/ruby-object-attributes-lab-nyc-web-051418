aa888c378685186e2bb9e955a1aafe64385a0359
squamuglia <maxsmouha@gmail.com>
2018-05-15 08:57:00 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+  def name_init(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    "#{@name}"
+  end
+
+  def breed_init(breed)
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    "#{@breed}"
+end
+end
